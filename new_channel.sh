8a6fe454bede861c0e7a14d305842579832221ac
and189 <and1_89@live.de>
2020-03-28 19:33:33 +0100

    Update new_channel.sh

--- new_channel.sh
+++ new_channel.sh
@@ -15,7 +15,7 @@ linkapi=$api"channels.createChannel?data[broadcast]=0&data[title]=$channel&data[
 link=$linkapi
 
 sudo wget -O /var/www/html/raid_bot/${modus}.txt $link
-channelid=$(cat json.txt | grep  channel_id |awk -F ": "'|'","'' '{ print $2 }' | uniq )
+channelid=$(cat ${modus}.txt | grep  channel_id |awk -F ": "'|'","'' '{ print $2 }' | uniq )
 linkapi2=$api"channels.editAdmin?data%5Bchannel%5D=-100$channelid&data%5Buser_id%5D=%40$admin&data%5Badmin_rights%5D%5B_%5D=chatAdminRights&data%5Badmin_rights%5D%5Bchange_info%5D=1&$
 sudo wget -O /var/www/html/raid_bot/jsonnew_channel.txt $linkapi2
 
