8e2a436879869a039c7c54e581a89a6425f09962
and189 <and1_89@live.de>
2020-03-28 12:03:10 +0100

    Create new_channel.sh

---
+++ new_channel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#### new_channel.sh / arename/  modus /  admin without @ /api
+
+
+areaname=$1
+modus=$2
+admin=$3
+api=$4
+channel="$areaname+$modus" 
+channel_info="RemstalmapSupoort+https://t.me/RemstalMap"  
+linkapi=$api"channels.createChannel?data[broadcast]=0&data[title]=$channel&data[about]=$channel_info"
+link=$linkapi
+
+sudo wget -O /var/www/html/raid_bot/${modus}.txt $link
+channelid=$(cat json.txt | grep  channel_id |awk -F ": "'|'","'' '{ print $2 }' | uniq )
+linkapi2=$api"channels.editAdmin?data%5Bchannel%5D=-100$channelid&data%5Buser_id%5D=%40$admin&data%5Badmin_rights%5D%5B_%5D=chatAdminRights&data%5Badmin_rights%5D%5Bchange_info%5D=1&$
+sudo wget -O /var/www/html/raid_bot/jsonnew_channel.txt $linkapi2
+
+
