4c0e8f80de74eb393a8fa541359b6521e0afbc5b
and189 <and1_89@live.de>
2020-03-29 12:42:21 +0200

    Update new_channel.sh

--- new_channel.sh
+++ new_channel.sh
@@ -16,7 +16,7 @@ link=$linkapi
 
 sudo wget -O /var/www/html/raid_bot/${modus}.txt $link
 channelid=$(cat ${modus}.txt | grep  channel_id |awk -F ": "'|'","'' '{ print $2 }' | uniq )
-linkapi2=$api"channels.editAdmin?data%5Bchannel%5D=-100$channelid&data%5Buser_id%5D=%40$admin&data%5Badmin_rights%5D%5B_%5D=chatAdminRights&data%5Badmin_rights%5D%5Bchange_info%5D=1&$
+linkapi2=$api"channels.editAdmin?data%5Bchannel%5D=-100$channelid&data%5Buser_id%5D=%40$admin&data%5Badmin_rights%5D%5B_%5D=chatAdminRights&data%5Badmin_rights%5D%5Bchange_info%5D=1&data%5Badmin_rights%5D%5Bpost_messages%5D=1&data%5Badmin_rights%5D%5Bedit_messages%5D=1&data%5Badmin_rights%5D%5Bdelete_messages%5D=1&data%5Badmin_rights%5D%5Bban_users%5D=1&data%5Badmin_rights%5D%5Binvite_users%5D=1&data%5Badmin_rights%5D%5Bpin_messages%5D=1&data%5Badmin_rights%5D%5Badd_admins%5D=1
 sudo wget -O /var/www/html/raid_bot/jsonnew_channel.txt $linkapi2
 
 
